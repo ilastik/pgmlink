85359e97c6260ad98689288a29ead52effe897bc
ilastikdev <janez.ales@iwr.uni-heidelberg.de>
2016-05-10 18:13:14 +0200

    proper pgmlink_export

--- include/pgmlink/pgmlink_export.h
+++ include/pgmlink/pgmlink_export.h
@@ -1,8 +1,17 @@
 #ifndef PGMLINK_EXPORT_H
 #define PGMLINK_EXPORT_H
 
-#define PGMLINK_EXPORT
-#define PGMLINK_NO_EXPORT
+#ifdef _WIN32
+	#ifdef pgmlink_EXPORTS
+		#define PGMLINK_EXPORT __declspec(dllexport)
+	#elif defined(PGMLINK_STATIC_LIB)
+		#define PGMLINK_EXPORT
+	#else
+		#define PGMLINK_EXPORT __declspec(dllimport)
+	#endif
+#else
+	#define PGMLINK_EXPORT
+#endif
 
 #endif /* PGMLINK_EXPORT_H */
 
